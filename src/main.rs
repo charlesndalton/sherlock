217800be8d8d7b5e178d5d9a5fe6a93addaab0f5
charlesndalton <charlesndalton@gmail.com>
2022-11-10 13:40:44 +0800

    \"complete\" the script (hack it together into a very ugly but semi-workable state)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::net::UdpSocket;
+use std::net::Ipv4Addr;
 use anyhow::Result;
 use rand::random;
 
 const GOOGLE_PUBLIC_DNS: &str = "8.8.8.8";
 const DNS_UDP_PORT: u16 = 53;
 
+#[derive(Debug)]
 enum Type {
     A = 1,
     NS = 2,
@@ -13,7 +15,7 @@ enum Type {
 
 fn main() -> Result<()> {
     let socket = UdpSocket::bind("192.168.0.51:34258")?;
-    socket.connect((GOOGLE_PUBLIC_DNS, DNS_UDP_PORT))?;
+    socket.connect(("192.168.0.1", DNS_UDP_PORT))?;
 
     let labels = vec!["amazon", "com"];
 
@@ -55,6 +57,79 @@ fn main() -> Result<()> {
     message.append(&mut question);
     
     socket.send(&message)?;
+
+    let mut buf = [0; 1000];
+    match socket.recv(&mut buf) {
+        Ok(received) => {
+            println!("received {received} bytes {:?}", &buf[..received]);
+            let res_header = &buf[0..12];
+            let res_id = u16::from_be_bytes(res_header[0..2].try_into()?);
+            assert!(res_id == id);
+
+            let res_question_count = u16::from_be_bytes(res_header[4..6].try_into()?);
+            let res_answer_count = u16::from_be_bytes(res_header[6..8].try_into()?);
+
+            println!("question count: {}, answer count: {}", res_question_count, res_answer_count);
+
+            let mut buf_p = 12;
+            for _ in [0..res_question_count] {
+                loop {
+                    let tag_len = buf[buf_p];
+                    buf_p += 1;
+
+                    if tag_len == 0 {
+                        break;
+                    }
+
+                    buf_p += tag_len as usize;
+                }
+                buf_p += 4;
+            }
+
+            for _ in [0..res_answer_count] {
+                loop {
+                    buf_p += 2;
+
+                    let res_type = match u16::from_be_bytes(buf[buf_p..buf_p+2].try_into()?) {
+                        1 => Type::A,
+                        2 => Type::NS,
+                        5 => Type::CNAME,
+                        _ => unimplemented!(),
+                    };
+
+                    buf_p += 2;
+
+                    // lol using a struct for the type but using a string for the class...
+                    // this really is garbage code :shrug:
+                    let res_class = match u16::from_be_bytes(buf[buf_p..buf_p+2].try_into()?) {
+                        1 => "Internet",
+                        _ => unimplemented!(),
+                    };
+
+                    buf_p += 2;
+
+                    let res_ttl = u32::from_be_bytes(buf[buf_p..buf_p+4].try_into()?);
+
+                    buf_p += 4;
+
+                    let res_rd_len = u16::from_be_bytes(buf[buf_p..buf_p+2].try_into()?);
+
+                    buf_p += 2;
+
+                    let ip_addr = Ipv4Addr::new(buf[buf_p], buf[buf_p+1], buf[buf_p+2], buf[buf_p+3]);
+
+                    buf_p += 4;
+
+                    println!("RECORD OF TYPE {:?} WITH CLASS {:?} AND TTL {:?}, IP ADDR: {:?}", res_type, res_class, res_ttl, ip_addr);
+                } 
+            }
+
+
+
+
+        },
+        Err(e) => println!("recv function failed: {e:?}"),
+    }
 
     Ok(())
 }
